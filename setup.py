b08e81f9134f52e68eb796969ba80fa816b7c180
Ran Gilad-Bachrach <12262288+ranigb@users.noreply.github.com>
2024-09-12 00:07:09 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,25 +4,32 @@ setup(
   packages = ['xgbGAMView'],   
   version = '0.1',      
   license='MIT',        
-  description = 'Generalized Additive Models (GAMs) trained using xgboost with visualization and smoothing',   # Give a short description about your library
-  author = 'MLWell lab, Tel-Aviv University',                   # Type in your name
-  author_email = 'rgb@tauex.tau.ac.il',      # Type in your E-Mail
+  description = 'Generalized Additive Models (GAMs) trained using xgboost with visualization and smoothing',   
+  author = 'MLWell lab, Tel-Aviv University', 
+  author_email = 'rgb@tauex.tau.ac.il',  
   url = 'https://github.com/TAU-MLwell/xgbGAMView',  
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Generalized Additive Model', 'GAM', 'explainable AI', "xAI"],   # Keywords that define your package best
+  download_url = 'https://github.com/TAU-MLwell/xgbGAMView/archive/refs/tags/0.1.tar.gz',   
+  keywords = ['Generalized Additive Model', 'GAM', 'explainable AI', "xAI"],  
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+        'numpy',
+        'pandas',
+        'matplotlib.pyplot',
+        'xgboost',
+        'json',
+        'os',
+        'matplotlib.colors',
+        'sklearn.neighbors',
+        'sklearn.base',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',    
 
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',    
     'Topic :: Software Development :: Build Tools',
 
     'License :: OSI Approved :: MIT License',   
 
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
