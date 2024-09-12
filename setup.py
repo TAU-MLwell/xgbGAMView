1f5def04411ee71be3255c4b301d82ee3c9e8773
Ran Gilad-Bachrach <12262288+ranigb@users.noreply.github.com>
2024-09-12 09:01:12 +0300

    Update setup.py to read readme.md

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from distutils.core import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
   name = 'xgbGAMView',     
   packages = ['xgbGAMView'],   
   version = '0.1',      
   license='MIT',        
   description = 'Generalized Additive Models (GAMs) trained using xgboost with visualization and smoothing',   
+  long_description=long_description,
+  long_description_content_type='text/markdown'
   author = 'MLWell lab, Tel-Aviv University', 
   author_email= 'rgb@tauex.tau.ac.il',
   url = 'https://github.com/TAU-MLwell/xgbGAMView',  
